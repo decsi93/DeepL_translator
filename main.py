ffb96d2cf2ae0f2354fd9523234699000383ae74
decsi93 <47918637+decsi93@users.noreply.github.com>
2024-05-16 08:28:30 +0200

    1st

--- main.py
+++ main.py
@@ -4,79 +4,11 @@ from datetime import datetime
 from search import search
 from logging import basicConfig, getLogger, DEBUG
 flag = 1
-
-def screen_size(parameter):
-    root = tk.Tk()
-    root.withdraw()
-
-    if parameter == "width" or "w":
-        width = root.winfo_screenwidth()
-        root.destroy()
-            return width
-
-        elif parameter == "height" or "h":
-            height = root.winfo_screenheight()
-            root.destroy()
-            return height
-
-    # Create main window
-    window = tk.Tk()
-    window.title("DeepL Translator")
-    window.geometry(f"{screen_size("w")//2}x{screen_size("h")//2}")
-
-    # Input path
-    print(flag)
-    input_label = tk.Label(window, text="Fordítandó PDF-ek teljes neve:" if flag == 1 else "Fordítandó PDF teljes neve:")
-    input_label.pack()
-    input_var = tk.StringVar()
-    input_entry = tk.Entry(window, textvariable=input_var)
-    input_entry.pack()
-
-    def update():
-        global input_label
-        if check_box_status.get():
-            text = "Fordítandó PDF-ek teljes neve:"
-
-        else:
-            text = "Fordítandó PDF teljes neve:"
-        window.mainloop()
-        input_label = tk.Label(window, text=text)
-        return input_label.pack()
-
-    check_box_status = tk.IntVar(value=1)
-    check_box = tk.Checkbutton(window, text="Több PDF fordítása", variable=check_box_status,
-                               onvalue=1, offvalue=0, command=update)
-    check_box.pack()
-
-    # Output path
-    output_label = tk.Label(window, text="Kimeneti file:")
-    output_label.pack()
-    output_var = tk.StringVar()
-    output_entry = tk.Entry(window, textvariable=output_var)
-    output_entry.pack()
-
-    # Translate button
-    translate_button = tk.Button(window, text="Küldés", command=translate)
-    translate_button.pack()
-
-    # Error display (after)
-    after_var = tk.StringVar()
-    after_label = tk.Label(window, textvariable=after_var)
-    after_label.pack()
-
-    # Error display (during)
-    during_var = tk.StringVar()
-    during_label = tk.Label(window, textvariable=during_var)
-    during_label.pack()
-
-    # Run the main event loop
-    window.mainloop()
-
-
 # Initialize translator
 translator = Translator("4ec2251b-bd81-4a7a-bcd4-cb9366d4e0bb:fx")
-
-after_var, during_var, input_var = tk.StringVar()
+after_var = tk.StringVar()
+during_var = tk.StringVar()
+input_var = tk.StringVar()
 
 
 def logger():
@@ -111,4 +43,70 @@ def translate():
         print(f"{during_var}\t{datetime}", file=logs)
         logs.close()
 
-tkinter()
+
+def screen_size(parameter):
+    root = tk.Tk()
+    root.withdraw()
+
+    if parameter == "width" or "w":
+        width = root.winfo_screenwidth()
+        root.destroy()
+        return width
+
+    elif parameter == "height" or "h":
+        height = root.winfo_screenheight()
+        root.destroy()
+        return height
+
+# Create main window
+window = tk.Tk()
+window.title("DeepL Translator")
+window.geometry(f"{screen_size("w")//2}x{screen_size("h")//2}")
+
+# Input path
+print(flag)
+input_label = tk.Label(window, text="Fordítandó PDF-ek teljes neve:" if flag == 1 else "Fordítandó PDF teljes neve:")
+input_label.pack()
+input_var = tk.StringVar()
+input_entry = tk.Entry(window, textvariable=input_var)
+input_entry.pack()
+
+def update():
+    global input_label
+    if check_box_status.get():
+        text = "Fordítandó PDF-ek teljes neve:"
+
+    else:
+        text = "Fordítandó PDF teljes neve:"
+
+    window.mainloop()
+    input_label = tk.Label(window, text=text)
+
+check_box_status = tk.IntVar(value=1)
+check_box = tk.Checkbutton(window, text="Több PDF fordítása", variable=check_box_status,
+                               onvalue=1, offvalue=0, command=update)
+check_box.pack()
+
+# Output path
+output_label = tk.Label(window, text="Kimeneti file:")
+output_label.pack()
+output_var = tk.StringVar()
+output_entry = tk.Entry(window, textvariable=output_var)
+output_entry.pack()
+
+# Translate button
+translate_button = tk.Button(window, text="Küldés", command=translate)
+translate_button.pack()
+
+# Error display (after)
+after_var = tk.StringVar()
+after_label = tk.Label(window, textvariable=after_var)
+after_label.pack()
+
+# Error display (during)
+during_var = tk.StringVar()
+during_label = tk.Label(window, textvariable=during_var)
+during_label.pack()
+
+# Run the main event loop
+window.mainloop()
