1d93825a9716cd89917a974210912ea01581dc5d
cupboard008 <168560648+cupboard008@users.noreply.github.com>
2024-07-05 16:24:31 +0200

    Beta 4.0

--- setup.py
+++ setup.py
@@ -1,4 +1,49 @@
-from os import path, mkdir
+# from itertools import product
+from string import ascii_letters, digits, punctuation
+
+ls = str(digits + ascii_letters + punctuation)
+print(len(ls))
+
+byt = 20497442737673217636335254372662635343505226212781934945547756536032971210714044111662233018898784996397633088879354537020160
+
+
+prefixes = {
+    "kilo": 0.001,
+    "mega": 0.000_001,
+    "giga": 0.000_000_001,
+    "tera": 0.000_000_000_001,
+    "peta": 1.000_000e-15,
+    "exa": 1.000_000e-18,
+    "zeta": 1.000_000e-21,
+    "yota": 1.000_000e-24
+}
+
+# print(byt / giga)
+
+# Define the base and the exponents
+base = 94
+first_exponent = 8
+last_exponent = 63
+number_of_terms = last_exponent - first_exponent + 1
+common_ratio = base
+
+# Calculate 94^8
+a = base ** first_exponent
+
+# Calculate 94^56
+r_n = base ** number_of_terms
+
+# Calculate the sum of the geometric series
+S = a * (r_n - 1) // (common_ratio - 1)
+
+for exponent in range(8, 21):
+    size = base ** exponent
+    for name, num in prefixes.items():
+        print(f"{exponent}\t{size} in bytes")
+        print(f"{exponent}\t{size * num} in {name}bytes\n")
+
+
+"""from os import path, mkdir
 from subprocess import run, PIPE, CalledProcessError
 directories = ["Raw", "Done", ".In_progress"]
 
@@ -32,7 +77,6 @@ for file_path in directories:
     check_prep(file_path)
 
 
-""""
-SW: 2.0.32
-IP: 172.28.21.119
 """
+# SW: 2.0.32
+# IP: 172.28.21.119
